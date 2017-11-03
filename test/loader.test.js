4edefdc77da76793a8a782443b83091dae7bc155
Vinicius Pacheco <vin175pacheco@gmail.com>
2017-11-03 16:55:18 -0200

    Fixed source maping

--- test/loader.test.js
+++ test/loader.test.js
@@ -5,7 +5,7 @@ describe('Loader', () => {
     await parse('.home-asd {\n\tcolor: #333;\n}\n', null, { sourceMap: true })
       .then(result => expect(result).toEqual(`
 exports = module.exports = [];
-exports.push([module.i, "._home-asd_16pq3_1 {\\n\tcolor: #333;\\n}\\n"]);
+exports.push([module.i, "._home-asd_16pq3_1 {\\n\\tcolor: #333;\\n}\\n"]);
 exports.locals = {"home-asd":"_home-asd_16pq3_1"};`.trim()))
   })
 })
