c6151bcbafb1496e206e19f569effcc1e2e27e5d
Vinicius Pacheco <vin175pacheco@gmail.com>
2017-11-03 16:17:55 -0200

    Changed name + Fix source mapping + Add simple test

---
+++ test/helpers.js
@@ -0,0 +1,28 @@
+import cssModularLoader from '../lib/loader'
+
+export function runLoader(loader, input, map, options, callback) {
+  loader.call({
+    callback,
+    async: () => callback,
+    options: Object.assign({ context: '' }, options),
+    loaders: [{ request: '/path/css-loader' }],
+    loaderIndex: 0,
+    context: '',
+    resource: 'test.css',
+    resourcePath: 'test.css',
+    request: 'css-loader!test.css',
+    emitError: (message) => { throw new Error(message) }
+  }, input, map)
+}
+
+export function parse(input, map, options) {
+  return new Promise((resolve, reject) => {
+    runLoader(cssModularLoader, input, map, options, (error, result) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
