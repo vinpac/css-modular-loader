7b408e053c6d7dd8240e4c63c00407ab27660adc
Vinicius Pacheco <vin175pacheco@gmail.com>
2018-01-15 21:33:01 -0200

    Fix localsLoader

--- test/loader.test.js
+++ test/loader.test.js
@@ -3,12 +3,12 @@ import { parse } from './helpers'
 describe('Loader', () => {
   it('should work', async () => {
     await parse(
-      '.home-asd {\n\tcolor: #333; background: url(\'./image.png\');\n}\n',
+      '@use .c as .a from \'./a\'; .home-asd {\n\tcolor: #333; background: url(\'./image.png\');\n}\n',
       null,
       { sourceMap: true },
     ).then(result => expect(result).toEqual(`
 exports = module.exports = [];
-exports.push([module.i, "._home-asd_ddk2k_1 {\\n\\tcolor: #333; background: url(" + require("./image.png") + ");\\n}\\n"]);
-exports.locals = {"home-asd":"_home-asd_ddk2k_1"};`.trim()))
+exports.push([module.i, "._home-asd_1wtsu_1 {\\n\\tcolor: #333; background: url(" + require("./image.png") + ");\\n}\\n"]);
+exports.locals = {"a":"_c_1wtsu_1","home-asd":"_home-asd_1wtsu_1"};`.trim()))
   })
 })
