08b01530aa1bb7fe812d27c225a919a1f67a6e24
Vinicius Pacheco <vin175pacheco@gmail.com>
2018-01-30 01:02:31 -0200

    Add support to extension query option + Add path resolver to handle webpack alias

--- test/helpers.js
+++ test/helpers.js
@@ -1,18 +1,34 @@
 import cssModularLoader from '../lib/loader'
 
 export function runLoader(loader, input, map, options, callback) {
-  loader.call({
-    callback,
-    async: () => callback,
-    options: Object.assign({ context: '' }, options),
-    loaders: [{ request: '/path/css-loader' }],
-    loaderIndex: 0,
-    context: '',
-    resource: 'test.css',
-    resourcePath: 'test.css',
-    request: 'css-loader!test.css',
-    emitError: message => { throw new Error(message) }
-  }, input, map)
+  loader.call(
+    {
+      callback,
+      async: () => callback,
+      options: Object.assign(
+        {
+          resolve: {
+            alias: {
+              Components: '/home/Components',
+            },
+          },
+          context: '',
+        },
+        options,
+      ),
+      loaders: [{ request: '/path/css-loader' }],
+      loaderIndex: 0,
+      context: '',
+      resource: 'test.css',
+      resourcePath: 'test.css',
+      request: 'css-loader!test.css',
+      emitError: message => {
+        throw new Error(message)
+      },
+    },
+    input,
+    map,
+  )
 }
 
 export function parse(input, map, options) {
